22ba324113d9ec1172e2cd99d0b05322693d66c3
AJ <aj@Alexandras-MacBook-Pro-2.local>
2019-08-10 14:45:00 -0400

    All required elements present

--- main.js
+++ main.js
@@ -1,18 +1,90 @@
+let originalURL = 'https://docs.google.com/spreadsheets/d/1dtenl7Cc9R_UcKxnYvCeOFT9IaOwFw46JcxnquZYR_k/edit#gid=0'
 
-const slideIndex = 1;
-showDivs(slideIndex);
+let id = '1dtenl7Cc9R_UcKxnYvCeOFT9IaOwFw46JcxnquZYR_k'
 
-const plusDivs = (n) => {
-  showDivs(slideIndex += n);
+let source = 'https://spreadsheets.google.com/feeds/list/1dtenl7Cc9R_UcKxnYvCeOFT9IaOwFw46JcxnquZYR_k/od6/public/values?alt=json'
+
+fetch(source)
+  .then( res => res.json())
+  .then( data => {
+    console.log('this is data feed', data.feed.entry)
+    let projects = data.feed.entry.map( d => {
+      return {
+        title: d.gsx$title.$t,
+        image: d.gsx$image.$t,
+        description: d.gsx$description.$t
+      }
+  })
+  console.log('this is projects', projects);
+  createCards(projects)
+})
+
+// all this down
+
+class Card {
+  constructor(obj) {
+    this.title = obj.title
+    this.image = obj.image
+    this.description = obj.description
+  }
+
+  render() {
+    const item = document.createElement('div')
+    item.classList.add('carousel-item')
+
+    const col = document.createElement('div')
+    col.classList.add('col');
+
+    const card = document.createElement('div')
+    card.classList.add('card');
+
+    const cardImage = document.createElement('div')
+    cardImage.classList.add('card-image');
+
+    const image  = document.createElement('img')
+    image.setAttribute('src', this.image)
+
+    const cardTitle = document.createElement('span')
+    cardTitle.classList.add('card-title');
+    cardTitle.innerText = this.title
+
+    const cardContent = new CardContent(this.description)
+    console.log('this is cardConten ', cardContent)
+    cardImage.appendChild(image)
+    card.appendChild(cardTitle)
+    card.appendChild(cardImage)
+
+    card.appendChild(cardContent.render())
+    col.appendChild(card)
+    item.appendChild(col)
+
+    return item
+  }
 }
 
-const showDivs = (n) => {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+class CardContent {
+  constructor(desc) {
+    this.desc = desc
   }
-  x[slideIndex-1].style.display = "block";
+  render () {
+    console.log('this is desc', this.desc)
+    const cardContent = document.createElement('div')
+    cardContent.classList.add('card-content');
+
+    const paragraph = document.createElement('p')
+    paragraph.innerText = this.desc
+
+    cardContent.appendChild(paragraph)
+    return cardContent
+  }
+}
+
+function createCards(projects){
+  const projectDiv = document.querySelector('.carousel-inner')
+  projects.forEach( obj => {
+    let card = new Card(obj)
+    console.log('this is card', card)
+    projectDiv.appendChild(card.render())
+  })
+
 }
